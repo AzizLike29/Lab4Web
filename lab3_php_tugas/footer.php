bfd640c95c6562c49f5977250975b66541a764b5
AzizLike29 <abdulfirdaus590@gmail.com>
2023-03-29 23:15:43 +0700

    Tugas pertemuan 4

---
+++ lab3_php_tugas/footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <p>&copy; 2023, Teknik Informatika, <a href="https://wa.me/62895618748150" style="text-decoration: none;">Abdul Aziz Firdaus</a>, Universitas Pelita Bangsa</p>
+</footer>
+</div>
+</body>
+
+</html>
